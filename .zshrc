3f6cbbfee8698646fff1fe51322d9cdae932aae6
Billy Le <hi@billyle.dev>
2024-10-30 11:33:46 +0800

    updated install script

--- .zshrc
+++ .zshrc
@@ -70,3 +70,7 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg --work-tree=$HOME'
 
 # Shell integrations
 source <(fzf --zsh)
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -8,12 +8,23 @@ if [ "$OS" != 'Darwin' ]; then
 	exit 1
 fi
 
+# Check if Command Line Tools are installed
+if ! xcode-select -p &>/dev/null; then
+    echo "Command Line Tools not found. Installing..."
+    xcode-select --install
+else
+    echo "Command Line Tools are already installed."
+fi
+
+# Check if Homebrew is installed
 if ! command -v brew &> /dev/null; then
 	echo "Homebrew is not installed. Installing now..."
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	
 	if command -v brew &> /dev/null; then
 		echo "Homebrew installed successfully."
+		echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+		eval "$(/opt/homebrew/bin/brew shellenv)"
 	else
 		echo "Homebrew installation failed."
 		exit 1
@@ -45,6 +56,8 @@ formulas=(
 	"fzf"
 	"neovim"
 	"git"
+	"ripgrep"
+	"wget"
 )
 
 # Apps I use
@@ -65,3 +78,27 @@ echo "Installing casks..."
 for cask in "${casks[@]}"; do
 	check_and_install "$cask" "--cask"
 done
+
+# Install Node Version Manager
+export NVM_DIR="$HOME/.nvm"
+(
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR" &&
+  cd "$NVM_DIR" &&
+  git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))"
+)
+
+# Add NVM to .zshrc and load NVM
+NVM_LINES='
+# NVM configuration
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+'
+
+if ! grep -q 'NVM configuration' "$HOME/.zshrc"; then
+    echo "$NVM_LINES" >> "$HOME/.zshrc"
+    echo "NVM lines added to .zshrc."
+    source "$HOME/.zshrc"
+else
+    echo "NVM lines already exist in .zshrc."
+fi
